da6ce83361f9934568133ae7b90b3add6767d85c
Aman Kubanychbek <amankubanychbek@gmail.com>
2018-09-19 00:16:44 +0300

    refactor material-ui imports

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,10 +1,12 @@
 // @flow
 import * as React from 'react'
-import Button from '@material-ui/core/Button'
-import TextField from '@material-ui/core/TextField'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import {Checkbox} from '@material-ui/core'
+import {
+  Button,
+  Checkbox,
+  List,
+  ListItem,
+  TextField,
+} from '@material-ui/core'
 
 import {
   uid,
